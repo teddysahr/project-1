2aa38e976b3de07133d6a62b63d6e334371540d7
Christopher Garcia <chrisgarcia0903@gmail.com>
2021-10-27 12:40:45 -0400

    Saves  what the user inputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,29 @@
 // DEPENDENCIES ============================
 var movieSearch = $("#movie-search");
 var generateButton = $("#generate-button");
+var submitBtn = $("#search-button")
+
 console.log(movieSearch);
 console.log(generateButton);
+submitBtn.on('click', function(){
+    var input = document.getElementById("movie-search").value;
+    console.log(input)
+})
 
 // Watchmode API
-fetch("https://watchmode.p.rapidapi.com/sources/?types=sub%2Cfree&regions=US%2CCA", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "watchmode.p.rapidapi.com",
-		"x-rapidapi-key": "e6b61b06acmsh9f6def52697ec71p1fb2acjsn1e7a6eefdc13"
-	}
-})
-.then(response => {
-	console.log(response.status);
-})
-.catch(err => {
-	console.error(err);
-});
+// fetch("https://watchmode.p.rapidapi.com/sources/?types=sub%2Cfree&regions=US%2CCA", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "watchmode.p.rapidapi.com",
+// 		"x-rapidapi-key": "e6b61b06acmsh9f6def52697ec71p1fb2acjsn1e7a6eefdc13"
+// 	}
+// })
+// .then(response => {
+// 	console.log(response.status);
+// })
+// .catch(err => {
+// 	console.error(err);
+// });
+
+
 
