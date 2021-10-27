3db70bb5d93d7d679099e4f7541a471e8a419b4b
Mahamadou Kabba <mkabba34@gmail.com>
2021-10-27 11:21:33 -0400

    second api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,3 @@
-// DEPENDENCIES ============================
-var movieSearch = $("#movie-search");
-var generateButton = $("#generate-button");
-console.log(movieSearch);
-console.log(generateButton);
-
-// Watchmode API
-fetch(
-  "https://watchmode.p.rapidapi.com/sources/?types=sub%2Cfree&regions=US%2CCA",
-  {
-    method: "GET",
-    headers: {
-      "x-rapidapi-host": "watchmode.p.rapidapi.com",
-      "x-rapidapi-key": "e6b61b06acmsh9f6def52697ec71p1fb2acjsn1e7a6eefdc13",
-    },
-  }
-)
-  .then((response) => {
-    console.log(response.status);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-
 // Data-Imdb API
 
 fetch("https://data-imdb1.p.rapidapi.com/movie/order/upcoming/?page_size=50", {
