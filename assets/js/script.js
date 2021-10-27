1f653b3f5ea3afd63c7037014f176ad30ebb9150
Ami Asokumar <gasokumar@gmail.com>
2021-10-27 12:18:19 -0400

    Added second API to javascript file

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,22 @@ fetch(
   .catch((err) => {
     console.error(err);
   });
+
+// Data-Imdb API
+fetch("https://data-imdb1.p.rapidapi.com/movie/order/upcoming/?page_size=50", {
+  method: "GET",
+  headers: {
+    "x-rapidapi-host": "data-imdb1.p.rapidapi.com",
+    "x-rapidapi-key": "d364edfff6msh01f109871a1d8c0p1303aejsnc909ac339a37",
+  },
+})
+  .then((response) => {
+    console.log(response.status);
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
