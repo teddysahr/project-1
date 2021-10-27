1511fd1dafb69c5e97d2388195b708930f3bda4d
Ami Asokumar <gasokumar@gmail.com>
2021-10-26 20:09:49 -0400

    syncing changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 // DEPENDENCIES ============================
 var movieSearch = $("#movie-search");
 var generateButton = $("#generate-button");
@@ -6,17 +5,19 @@ console.log(movieSearch);
 console.log(generateButton);
 
 // Watchmode API
-fetch("https://watchmode.p.rapidapi.com/sources/?types=sub%2Cfree&regions=US%2CCA", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "watchmode.p.rapidapi.com",
-		"x-rapidapi-key": "e6b61b06acmsh9f6def52697ec71p1fb2acjsn1e7a6eefdc13"
-	}
-})
-.then(response => {
-	console.log(response.status);
-})
-.catch(err => {
-	console.error(err);
-});
-
+fetch(
+  "https://watchmode.p.rapidapi.com/sources/?types=sub%2Cfree&regions=US%2CCA",
+  {
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "watchmode.p.rapidapi.com",
+      "x-rapidapi-key": "e6b61b06acmsh9f6def52697ec71p1fb2acjsn1e7a6eefdc13",
+    },
+  }
+)
+  .then((response) => {
+    console.log(response.status);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
