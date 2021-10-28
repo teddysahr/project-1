086f2b867fc921402a6908af2b8be37d2fe7886a
Ami Asokumar <gasokumar@gmail.com>
2021-10-28 14:53:51 -0400

    deleted unnecessary comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -101,32 +101,9 @@ submitBtn.on("click", function () {
       console.error(err);
     });
 });
-// function removeAllChildNodes(resultList) {
-//     while (resultList.listItem) {
-//         resultList.removeChild(listItem);
-//     }
-// }
+
 function clearPage() {
   resultList.empty();
   console.log(resultList);
   imageBox.empty();
 }
-
-// Data-Imdb API - For random movie
-// fetch("https://data-imdb1.p.rapidapi.com/movie/order/upcoming/?page_size=50", {
-//   method: "GET",
-//   headers: {
-//     "x-rapidapi-host": "data-imdb1.p.rapidapi.com",
-//     "x-rapidapi-key": "d364edfff6msh01f109871a1d8c0p1303aejsnc909ac339a37",
-//   },
-// })
-//   .then((response) => {
-//     console.log(response.status);
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log(data);
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//   });
